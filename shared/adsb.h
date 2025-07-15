6cd3ab729f6d6fa553cad8c87c7f30665c4ca3bb
Francisco Medinilla <medinillagarciafrancisco@gmail.com>
2025-07-15 12:49:53 +0200

    feat(shared): change how to encode adsb

--- shared/adsb.h
+++ shared/adsb.h
@@ -2,25 +2,11 @@
 #define ADSB_H
 
 #include <stdint.h>
+#include "flight.h"
 
-typedef struct _adsb_frame {
-    unsigned char DF : 5;       // Downlink Format (5 bits)
-    unsigned char CA : 3;       // Transponder Capacity (3 bits)
-    unsigned int ICAO : 24;     // ICAO address (24 bits)
-    uint64_t ME: 56;            // Message (56 bits)
-    uint32_t PI: 24;            // Parity Information (24 bits)
-} adsb_frame_t;
+const char *characters = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ##### ###############0123456789######";
 
-typedef struct _adsb_message {
-    unsigned char type : 5;     // Message type (5 bits)
-    uint64_t content : 51;      // Message content (51 bits)
-} adsb_message_t;
-
-void adsb_create_frame(adsb_frame_t *frame);
-void adsb_set_icao(adsb_frame_t *frame, unsigned int icao);
-void adsb_set_message(adsb_frame_t *frame, adsb_message_t message);
-void adsb_pack_frame(const adsb_frame_t *frame, uint8_t *packed);
-void adsb_print_frame(const adsb_frame_t *frame);
-void adsb_print_hex(const adsb_frame_t *frame);
+int adsb_identificaction_get_charpos(char c);
+char adsb_identificaction_get_char(int pos);
 
 #endif // ADSB_H
