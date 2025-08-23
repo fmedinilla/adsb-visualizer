cda6865bd1f9d1e07d5e0e65e5b8e66c7011ee8d
Francisco Medinilla <medinillagarciafrancisco@gmail.com>
2025-08-23 16:18:36 +0200

    refactor(generator): organize main file and use new functions

--- generator/src/main.c
+++ generator/src/main.c
@@ -1,7 +1,17 @@
+#include <stdlib.h>
+#include <pthread.h>
 #include "generator.h"
+#include "sender.h"
 
 int main(void)
 {
-    generator_start();
+    flight_data_t *flight_data = NULL;
+    const int NUM_FLIGHTS = read_ini_data(&flight_data);
+
+    pthread_t sender;
+    start_sender(&sender);
+
+    start_generator(flight_data, NUM_FLIGHTS, &sender);
+
     return 0;
 }
