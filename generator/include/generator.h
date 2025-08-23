b90dd31a78993e826a8b04950722f002d31172fe
Francisco Medinilla <medinillagarciafrancisco@gmail.com>
2025-08-23 16:15:38 +0200

    refactor(generator): reorganize generator functions

--- generator/include/generator.h
+++ generator/include/generator.h
@@ -1,6 +1,10 @@
 #ifndef GENERATOR_H
 #define GENERATOR_H
 
-void generator_start();
+#include <pthread.h>
+#include "flight.h"
+
+int read_ini_data(flight_data_t **flight_data);
+void start_generator(flight_data_t *flight_data, int num_flights, pthread_t *sender);
 
 #endif // GENERATOR_H
--- generator/src/generator.c
+++ generator/src/generator.c
@@ -4,102 +4,7 @@
 #include <unistd.h>
 #include "generator.h"
 #include "flight.h"
-
-typedef struct _flight_data {
-    int ICAO;
-    double lat;
-    double lon;
-    int alt;
-    int speed;
-    double track;
-    char callsign[9];
-} flight_data_t;
-
-typedef struct _message_queue {
-    flight_message_t messages[100];
-    int front;
-    int rear;
-    int count;
-    pthread_mutex_t mutex;
-    pthread_cond_t cond;
-} message_queue_t;
-
-message_queue_t message_queue;
-
-void queue_init(message_queue_t *q)
-{
-    q->front = 0;
-    q->rear = 0;
-    q->count = 0;
-    pthread_mutex_init(&q->mutex, NULL);
-    pthread_cond_init(&q->cond, NULL);
-}
-
-int enqueue_message(message_queue_t *q, flight_message_t *message)
-{
-    pthread_mutex_lock(&q->mutex);
-    if (q->count == 100) {
-        pthread_mutex_unlock(&q->mutex);
-        return -1; // Queue is full
-    }
-    memcpy(q->messages[q->rear], message, sizeof(flight_message_t));
-    q->rear = (q->rear + 1) % 100;
-    q->count++;
-    pthread_cond_signal(&q->cond);
-    pthread_mutex_unlock(&q->mutex);
-    return 0;
-}
-
-int dequeue_message(message_queue_t *q, flight_message_t *message)
-{
-    pthread_mutex_lock(&q->mutex);
-    while (q->count == 0) {
-        pthread_cond_wait(&q->cond, &q->mutex);
-    }
-    memcpy(message, &q->messages[q->front], sizeof(flight_message_t));
-    q->front = (q->front + 1) % 100;
-    q->count--;
-    pthread_mutex_unlock(&q->mutex);
-    return 0;
-}
-
-void *sender_func(void *args)
-{
-    flight_message_t message;
-    while(1) {
-        dequeue_message(&message_queue, &message);
-        flight_send_message(message);
-    }
-}
-
-void *flight_simulation(void *arg)
-{
-    flight_t *flight = (flight_t *)arg;
-
-    while (1) {
-        // update coordinates for simulation
-        flight_update_coordinates(flight);
-
-        // Get message
-        flight_message_t message;
-        flight_get_message(message, flight);
-
-        // Change message type for next iteration
-        if (flight->message_type == IDENTIFICATION_MESSAGE)
-            flight->message_type = POSITION_MESSAGE;
-        else
-            flight->message_type = IDENTIFICATION_MESSAGE;
-
-        // send message
-        enqueue_message(&message_queue, &message);
-        //flight_send_message(message);
-
-        // wait for 100 ms
-        usleep(100000);
-    }
-    
-    return NULL;
-}
+#include "message_queue.h"
 
 int read_ini_data(flight_data_t **flight_data)
 {
@@ -133,20 +38,12 @@ int read_ini_data(flight_data_t **flight_data)
     return num;  // Return the number of flights read
 }
 
-void generator_start()
+void start_generator(flight_data_t *flight_data, int num_flights, pthread_t *sender)
 {
-    pthread_t sender;
-
-    flight_data_t *flight_data = malloc(sizeof(flight_data_t));
-    const int NUM_FLIGHTS = read_ini_data(&flight_data);
-
-    flight_t flights[NUM_FLIGHTS];
-
+    flight_t flights[num_flights];
     queue_init(&message_queue);
 
-    pthread_create(&sender, NULL, sender_func, NULL);
-
-    for (int i = 0; i < NUM_FLIGHTS; i++) {
+    for (int i = 0; i < num_flights; i++) {
         flight_create(&flights[i],
             flight_data[i].ICAO,
             flight_data[i].callsign,
@@ -158,16 +55,14 @@ void generator_start()
         );
     }
 
-    free(flight_data);
-
     // create threads for each flight
-    for (int i = 0; i < NUM_FLIGHTS; i++) {
+    for (int i = 0; i < num_flights; i++) {
         pthread_create(&flights[i].thread, NULL, flight_simulation, (void *)&flights[i]);
     }
 
     // wait for all threads to finish
-    for (int i = 0; i < NUM_FLIGHTS; i++) {
+    for (int i = 0; i < num_flights; i++) {
         pthread_join(flights[i].thread, NULL);
     }
-    pthread_join(sender, NULL);
+    pthread_join(*sender, NULL);
 }
